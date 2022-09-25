4ea2ce656e115187ec248f2a64d63885c137d81e
AmeerAG <ameerag@AmeerAGs-MacBook-Pro.local>
2022-09-25 15:24:37 +0300

    InfoFillinVC UI

---
+++ VotingOnlineSystem0/VotingOnlineSystem0/View/DropMenuBtnView.swift
@@ -0,0 +1,34 @@
+//
+//  DropMenuBtnView.swift
+//  VotingOnlineSystem0
+//
+//  Created by AmeerAG on 23/09/2022.
+//
+
+import UIKit
+
+class DropMenuBtnView: UIButton {
+
+    override func draw(_ rect: CGRect) { // (ˇ) DRAW
+        let size = 14
+        let downImg = UIImageView(frame: CGRect(x: CGFloat(frame.size.width)-30, y: CGFloat((Int((frame.size.height))/2) - size/2 + 3), width: CGFloat(size), height: CGFloat(Double(size)-5)))
+        downImg.backgroundColor = .white.withAlphaComponent(0.0)
+        downImg.clipsToBounds = true
+        downImg.image = UIImage(systemName: "chevron.down")
+        addSubview(downImg)
+    }
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+    }
+
+    func customizeView(){
+        tintColor = .darkGray
+        layer.cornerRadius = 15
+        clipsToBounds = true
+    }
+    
+}
